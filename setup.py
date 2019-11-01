9fa2ef4ee7615f2c51e6a4336af707a08dd9cf6d
Ethan Shry <ethan.shry@wustl.edu>
2019-11-01 17:47:48 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('requirements.txt', 'r') as req:
+with open("requirements.txt", "r") as req:
     requirements = req.readlines()
 
 setuptools.setup(
     name="picar",
-    version="0.1.0",
+    version="0.1.1",
     author="Ethan Shry",
     author_email="ehshry@gmail.com",
     description="Platform to interface with Adeept Mars Rover for ESE 205",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ESE205/PiCar",
-    packages=['picar'],
-    install_requires=requirements
+    packages=["picar"],
+    install_requires=requirements,
 )
