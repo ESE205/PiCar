05d7ab4540adc2a0c2dab0268ab441cc05e1be0b
Ethan Shry <ehshry@gmail.com>
2019-11-19 17:47:16 -0600

    Ethan config (#1)
    * config and set servo refactor
    
    * add variables to __init__
    
    * typo
    
    * calc duty cycle pass self
    
    * write string to file
    
    * use config correctly
    
    * print on config

--- picar/__init__.py
+++ picar/__init__.py
@@ -1,1 +1,3 @@
-from picar.PiCar import PiCar
+from picar.PiCar import PiCar, PICAR_CONFIG_FILE_NAME, MOCK_CAR_CONFIG_FILE_NAME
+from picar.configure import configure_car
+from picar.test import execute_test
