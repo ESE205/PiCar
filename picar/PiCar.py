1681afca31bc6a03e171accb39d34eae4d261dc0
jdfeher <42353266+jdfeher@users.noreply.github.com>
2024-11-21 13:18:56 -0600

    Update PiCar.py - MPU6050
    Uncommented _MPU_Init() and added self._MPU_Init()

--- picar/PiCar.py
+++ picar/PiCar.py
@@ -141,6 +141,9 @@ class PiCar:
             clk=clk_pin, cs=cs_pin, miso=miso_pin, mosi=mosi_pin, gpio=gpio_adapter
         )
 
+        # intialize the MPU6050
+        self._MPU_Init()
+
         print("looking for servo configuration file...")
 
         if config_name is not None:
@@ -628,24 +631,24 @@ class PiCar:
     
 #    # ACCELEROMETER MPU-6050
 #    # Adapted from: https://www.electronicwings.com/raspberry-pi/mpu6050-accelerometergyroscope-interfacing-with-raspberry-pi
-#    def _MPU_Init(self):
-#        Device_Address = 0x68   # MPU6050 device address
-#        PWR_MGMT_1     = 0x6B
-#        SMPLRT_DIV     = 0x19
-#        CONFIG         = 0x1A
-#        GYRO_CONFIG    = 0x1B
-#        INT_ENABLE     = 0x38
-#
-#        #write to sample rate register
-#        self._bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
-#        #Write to power management register
-#        self._bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
-#        #Write to Configuration register
-#        self._bus.write_byte_data(Device_Address, CONFIG, 0)
-#        #Write to Gyro configuration register
-#        self._bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
-#        #Write to interrupt enable register
-#        self._bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+    def _MPU_Init(self):
+        Device_Address = 0x68   # MPU6050 device address
+        PWR_MGMT_1     = 0x6B
+        SMPLRT_DIV     = 0x19
+        CONFIG         = 0x1A
+        GYRO_CONFIG    = 0x1B
+        INT_ENABLE     = 0x38
+
+        #write to sample rate register
+        self._bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+        #Write to power management register
+        self._bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+        #Write to Configuration register
+        self._bus.write_byte_data(Device_Address, CONFIG, 0)
+        #Write to Gyro configuration register
+        self._bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+        #Write to interrupt enable register
+        self._bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
     def MPU_Read(self, val2read):
         # 1-xaccel, 2-yaccel, 3-zaccel, 4-xgyro, 5-ygyro, 6-zgyro
